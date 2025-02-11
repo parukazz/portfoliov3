af8f166e4754a7838cf3ad40046b0d0fea7afc77
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-11 15:56:04 +0700

    create footer-component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import bg from "../assets/img/bg-footer.jpeg";
+
+const Footer = () => {
+  return (
+    <footer className="relative bg-primary text-text-heading">asfassfas</footer>
+  );
+};
+
+export default Footer;
