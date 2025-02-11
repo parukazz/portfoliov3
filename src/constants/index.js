6da23e468bf2be0d4f03e58073452ec908a58fcb
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-11 22:43:13 +0700

    add social list

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const socialLinks = [
+  { id: 0, label: "instagram", url: "https://www.instagram.com/parukrev_/" },
+  { id: 1, label: "linkedin", url: "https://www.linkedin.com/in/parukazz" },
+  { id: 2, label: "github", url: "https://github.com/parukazz/" },
+  { id: 3, label: "behance", url: "https://www.behance.net/parukazziyi" },
+];
