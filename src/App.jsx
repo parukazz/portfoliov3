b5a68afecea3a2e0fe675e78ed71cf6edb3922d5
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-11 14:19:05 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="text-accent leading-120 w-desktop">
+      So I started to walk into the water. I won't lie to you boys, I was
+      terrified. But I pressed on, and as I made my way past the breakers a
+      strange calm came over me. I don't know if it was divine intervention or
+      the kinship of all living things but I tell you Jerry at that moment, I
+      was a marine biologist.
+    </div>
+  );
+};
 
-export default App
+export default App;
