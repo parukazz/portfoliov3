1595ae938f15e8564b6bdfe7fa759bc8d1e7d719
Paruk Azziyi <parukazziyi@gmail.com>
2025-02-12 21:21:39 +0700

    add menu list

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,11 @@ export const socialLinks = [
   { id: 2, label: "github", url: "https://github.com/parukazz/" },
   { id: 3, label: "behance", url: "https://www.behance.net/parukazziyi" },
 ];
+
+export const menuLinks = [
+  { id: 0, label: "home", path: "/" },
+  { id: 1, label: "works", path: "/works" },
+  { id: 2, label: "services", path: "/services" },
+  { id: 3, label: "about", path: "/about" },
+  { id: 4, label: "contact", path: "/contact" },
+];
